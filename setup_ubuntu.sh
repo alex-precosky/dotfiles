02e03a5e6650c358a57bf50a5f647d0121233ca8
Alex Precosky <precosky@gmail.com>
2025-06-28 15:19:26 -0700

    Fix mistakes in setup_ubuntu.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+set -e # terminate on failure
+
+sudo apt update
+sudo apt upgrade -y
+
 sudo apt install -y ansible
 
-ansible-galaxy collection install community.general
+ansible-galaxy collection install --force community.general
 ansible-playbook env_setup/playbook_ubuntu.yml
 ansible-playbook env_setup/playbook.yml
